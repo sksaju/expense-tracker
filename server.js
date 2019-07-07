cab6131a491208ff98610f15107ef0e57e027161
unknown <sksaaju@gmail.com>
2019-07-07 20:37:23 +0600

    connect with database

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require( 'express' );
+const morgan = require( 'morgan' );
+const cors = require( 'cors' );
+const bodyParser = require( 'body-parser' );
+const mongoose = require( 'mongoose' );
 
 const app = express();
+app.use( morgan( 'dev' ) );
+app.use( cors() );
+app.use( bodyParser.urlencoded( { extended: false } ) );
+app.use( bodyParser.json() );
+
 app.get( '/', ( req, res ) => {
     res.json({
         message: 'Welcome to our application'
@@ -10,4 +19,7 @@ app.get( '/', ( req, res ) => {
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () =>  {
     console.log( `SERVER is running on port ${PORT}` );
+    mongoose.connect( 'mongodb://localhost/transaction-management', { useNewUrlParser: true },  () => {
+        console.log( 'database connected' );
+    } )
 })
